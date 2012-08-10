5798a2c99f6d146e83a07d7de4f50a7c48ce794d
Alastair Rankine <alastair@girtby.net>
2012-08-10 13:55:11 -0500

    Support ninja build tool (http://martine.github.com/ninja/)

--- ede-cmake-build-tool.el
+++ ede-cmake-build-tool.el
@@ -15,7 +15,12 @@
    )
 )
 
-(defclass cmake-make-build-tool (ede-cmake-build-tool)
+(defclass cmake-makelike-build-tool (ede-cmake-build-tool)
+  ;; Base class for tools that work like make 
+  () )
+
+(defclass cmake-make-build-tool (cmake-makelike-build-tool)
+  ;; GNU Make and friends
   ((generator-string
     :type string
     :initarg :generator-string
@@ -23,14 +28,41 @@
    )
   )
 
+(defclass cmake-ninja-build-tool (cmake-makelike-build-tool)
+  ;; "Ninja is a small build system with a focus on speed"
+  ;; http://martine.github.com/ninja/
+  ((generator-string
+    :type string
+    :initarg :generator-string
+    :initform "Ninja")
+   )
+  )
+
 (defclass cmake-visual-studio-build-tool (ede-cmake-build-tool)
   ()
   )
 
-(defmethod get-target-directory ((this cmake-make-build-tool) project target)
+(defmethod get-target-directory ((this cmake-makelike-build-tool) project target)
   (let ((builddir (cmake-build-directory project))
         (path (oref target path)))
     (concat (file-name-as-directory builddir) (file-name-as-directory path))))
+
+(defmethod target-build-dir ((this cmake-makelike-build-tool) target)
+  "Returns the path to the build directory for target"
+  (concat (file-name-as-directory (cmake-build-directory (ede-target-parent target)))
+          (file-name-as-directory (oref target path))))
+
+(defmethod target-binary ((this cmake-makelike-build-tool) target)
+  "Returns the path to the binary for target"
+  (concat (target-build-dir this target) (oref target name)))
+
+(defmethod debug-target ((this cmake-makelike-build-tool) target)
+  "Invokes gdb to debug TARGET in BUILD-DIR"
+  (let* ((exe (target-binary this target))
+         (cmd (concat "gdb --annotate=3 " exe)))
+    (gdb cmd)))
+
+
 
 (defmethod invoke-make ((this cmake-make-build-tool) dir targetname)
   "Invokes make in DIR for TARGETNAME"
@@ -38,26 +70,11 @@
          (cmd (format "(cd %s ; make %s %s )" dir args targetname)))
     (compile cmd)))
 
-(defmethod target-build-dir ((this cmake-make-build-tool) target)
-  "Returns the path to the build directory for target"
-  (concat (file-name-as-directory (cmake-build-directory (ede-target-parent target)))
-          (file-name-as-directory (oref target path))))
-
-(defmethod target-binary ((this cmake-make-build-tool) target)
-  "Returns the path to the binary for target"
-  (concat (target-build-dir this target) (oref target name)))
-
 (defmethod compile-target-file ((this cmake-make-build-tool) target file)
   "Compiles FILE in BUILD-DIR in TARGET"
   (let* ((dir (target-build-dir this target))
          (doto (concat(file-name-sans-extension (file-name-nondirectory file)) ".o")))
     (invoke-make this dir doto)
     ))
-
-(defmethod debug-target ((this cmake-make-build-tool) target)
-  "Invokes gdb to debug TARGET in BUILD-DIR"
-  (let* ((exe (target-binary this target))
-         (cmd (concat "gdb --annotate=3 " exe)))
-    (gdb cmd)))
 
 (provide 'ede-cmake-build-tool)
